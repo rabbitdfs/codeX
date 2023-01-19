37342c813063b4da02568c9c6984800088386c1d
Miroslav28 <miroslav_ponomarenko@bk.ru>
2023-01-19 03:07:03 +0300

    ninth commit

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot's response
     
-    const response = await fetch('https://codex-hetx.onrender.com', {
+    const response = await fetch('https://codex-hetx.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
